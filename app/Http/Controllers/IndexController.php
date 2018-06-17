17ed2ba62d2db86ad208f6e150e656d0ca9c5cf5
saqo000000 <saqoxachatryan000000@gmail.com>
2018-06-17 23:16:08 +0400

    landing page 1commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\People;
+use App\Portfolio;
+use App\Client;
+use DB;
+use Mail;
+
+class IndexController extends Controller
+{
+    public function execute(Request $request){
+
+    	if($request->isMethod('post')){
+    		$messages = [
+    			'required' =>' :attribute Dasht@  petq e partadir lracnel',
+    			'email' =>' :attribute dashtum  grel email'
+    		];
+
+    		$this->validate($request,[
+    			'name'=>'required|max:255',
+    			'email'=>'required|email',
+    			'text'=>'required'
+    		], $messages);
+
+    		$data = $request->all();
+
+    		//to send mail
+    		$result = Mail::send('site.email',['data'=>$data], function($message) use ($data) {
+    			$mail_admin = env('MAIL_ADMIN');
+    			$message->from($data['email'],$data['name']);
+    			$message->to($mail_admin)->subject('Question');
+    		});
+    		
+
+    		if($result){
+    			return redirect()->route('home')->with('status','Email is send');
+    		}
+    	}
+
+    	$pages = Page::all();
+    	$portfolios = Portfolio::all(); 
+    	$services = Service::all();
+    	$peoples = People::all();
+    	$tags = DB::table('portfolios')->distinct()->pluck('filter');
+    	$clients = Client::all();
+
+    	$menu = array();
+    	foreach ($pages as $page) {
+    		$item = array('title'=>$page->name,'alias'=>$page->alias);
+    		array_push($menu, $item);
+    	}
+    	$item = array('title'=>'Services','alias'=>'service');
+    	array_push($menu, $item);
+
+    	$item = array('title'=>'Portfolios','alias'=>'Portfolio');
+    	array_push($menu, $item);
+
+    	$item = array('title'=>'Clients','alias'=>'clients');
+    	array_push($menu, $item);
+
+    	$item = array('title'=>'Team','alias'=>'team');
+    	array_push($menu, $item);
+
+    	$item = array('title'=>'Contact','alias'=>'contact');
+    	array_push($menu, $item);
+
+    		return view('site.index',array(
+    			'menu'=>$menu,
+    			'pages'=>$pages,
+    			'services'=>$services,
+    			'portfolios'=>$portfolios,
+    			'peoples'=>$peoples,
+    			'tags'=>$tags,
+    			'clients'=>$clients
+    		));
+
+    }
+}
---
+++ resources/views/admin/content_pages.blade.php
@@ -0,0 +1,38 @@
+<div style="0px 50px;">
+	@if($pages)
+	<table class="table table-hover table-striped">
+		<thead>
+			<tr>
+				<th>№ n/o</th>
+				<th>Name</th>
+				<th>Alias</th>
+				<th>Text</th>
+				<th>Create date</th>
+				<th>Delete</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($pages as $k => $page)
+				<tr>
+					<th>{{ $page->id }}</th>
+					<td>{!! Html::link(route('pagesEdit',['page'=>$page->id]), $page->name,['alt'=>$page->name]) !!}</td>
+					<td>{{ $page->alias }}</td>
+					<td>{{ $page->text }}</td>
+					<td>{{ $page->created_at }}</td>
+					<td>						
+						{!! Form::open([
+							'url'=>route('pagesEdit',['page'=>$page->id]),//action
+							'class'=>'form-horizontal',
+							'method'=>'post'
+							]) !!}  
+							{!! Form::hidden('action','delete') !!}
+							{!! Form::button('Delete',['class'=>'btn btn-danger','type'=>'submit']) !!}
+						{!! Form::close() !!}
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+	@endif
+	{!! Html::link(route('pagesAdd'),'New Page') !!}
+</div>
